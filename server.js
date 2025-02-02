b2f5eedf899a3360fff59b1dfe0080f9101418a2
Jgondev88html <thebullnot@gmail.com>
2025-02-02 05:12:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ wss.on('connection', (ws) => {
             const betAmount = data.amount;
 
             if (user.balance >= betAmount && user.attempts > 0) {
-                const win = Math.random() < 0.4; // 40% de probabilidad de ganar
+                const win = Math.random() < 0.6; // 60% de probabilidad de ganar
                 if (win) {
                     user.balance += betAmount;
                     user.consecutiveWins = (user.consecutiveWins || 0) + 1;
